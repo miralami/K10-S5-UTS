e32ceac3bcc0d1d890a4dcc74078762b251922b0
Afif Nur Sena <86998574+miralami@users.noreply.github.com>
2025-11-20 12:15:38 +0700

    Fix direktori grpc salah

--- websocket-service/grpc-server.js
+++ websocket-service/grpc-server.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PROTO_PATH = path.join(__dirname, '../proto/typing.proto');
+const PROTO_PATH = path.join(__dirname, '../shared/proto/typing.proto');
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
